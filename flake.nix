87f16ade213c07882e544f4a6360d577d76bbd13
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-20 10:55:37 +0400

    Allow unfree packages and add flake.lock

--- flake.nix
+++ flake.nix
@@ -15,7 +15,10 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
